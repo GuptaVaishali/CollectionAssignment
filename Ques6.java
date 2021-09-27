704e91052bb7615eadef22b07399336bdf44edf0
GuptaVaishali <vaishaligupta982@gmail.com>
2021-09-28 00:45:25 +0530

    updated

--- Ques6.java
+++ Ques6.java
@@ -1,8 +1,9 @@
+package Collection;
+
 import java.util.*;
-import java.util.stream.Collectors;
+import java.util.Map.Entry;
 
 public class Ques6 {
-    // Custom sort by element's frequency and index
     public static void sortByFrequencyAndIndex(int[] arr)
     {
         if (arr == null || arr.length < 2) {
@@ -18,22 +19,35 @@ public class Ques6 {
                 hm.put(arr[i],new Data(arr[i],1,i));
             }
             else{
-                int index = hm.get(arr[i]).index;
-                hm.put(arr[i],new Data(arr[i],++(hm.get(arr[i]).count),index));
+                int index = hm.get(arr[i]).getIndex();
+                int count = hm.get(arr[i]).getCount();
+                hm.put(arr[i],new Data(arr[i],++count,index));
             }
         }
 
-        // sort the values based on a custom comparator
-        List<Data> values = hm.values().stream()
-                .sorted()
-                .collect(Collectors.toList());
+        Set<Entry<Integer,Data>> set = hm.entrySet();
+        List<Entry<Integer,Data>> values = new ArrayList<>(set);
 
+        Collections.sort(values, new Comparator<Entry<Integer, Data>>() {
+            @Override
+            public int compare(Entry<Integer,Data> m1, Entry<Integer, Data> m2) {
+                // If two elements have different frequencies, then
+                // the one which has more frequency should come first
+                if (m1.getValue().getCount() != m2.getValue().getCount()) {
+                    return m2.getValue().getCount() - m1.getValue().getCount();
+                }
+
+                // If two elements have the same frequencies, then the
+                // one which has less index should come first
+                return m1.getValue().getIndex() - m2.getValue().getIndex();
+            }
+        });
 
         int k = 0;
-        for (Data data: values)
+        for (Entry<Integer,Data>  data: values)
         {
-            for (int j = 0; j < data.count; j++) {
-                arr[k++] = data.value;
+            for (int j = 0; j < data.getValue().getCount(); j++) {
+                arr[k++] = data.getValue().getElement();
             }
         }
     }
@@ -63,27 +77,27 @@ public class Ques6 {
     }
 }
 
-class Data implements Comparable<Data>
-{
-    int value, count, index;
+class Data {
+    private final int element;
+    private final int count;
+    private final int index;
 
-    public Data(int value, int count, int index)
-    {
-        this.value = value;
-        this.count = count;
-        this.index = index;
+    public int getElement() {
+        return element;
     }
 
-    public int compareTo(Data obj)
-    {
-        // If two elements have different frequencies, then
-        // the one which has more frequency should come first
-        if (this.count != obj.count) {
-            return obj.count - this.count;
-        }
+    public int getCount() {
+        return count;
+    }
 
-        // If two elements have the same frequencies, then the
-        // one which has less index should come first
-        return this.index - obj.index;
+    public int getIndex() {
+        return index;
+    }
+
+    public Data(int element, int count, int index)
+    {
+        this.element = element;
+        this.count = count;
+        this.index = index;
     }
 }
